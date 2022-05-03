6c52e2158843493fdaabdf3ec2aa59494ced9ac4
unknown <islam15-3879@diu.edu.bd>
2022-05-03 15:44:39 +0600

    data-json-showed

--- src/Pages/StockCars/StockCars.js
+++ src/Pages/StockCars/StockCars.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './StockCars.css'
 
 const StockCars = ({car}) => {
     const{img,name,quantity,description,price}=car;
+    const text=description.slice(0,75);
     return (
-        <div>
+        <div className='car-container'>
             <img src={img} alt="" />
             <h2>Name: {name}</h2>
             <h3>Price: {price}</h3>
             <h4>Quantity: {quantity}</h4>
-            <p>{description}</p>
+            <p>{text}</p>
+            <button className='button'>Update</button>
             
         </div>
     );
