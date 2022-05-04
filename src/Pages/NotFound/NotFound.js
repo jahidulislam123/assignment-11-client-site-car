ac3bfc6bfc88f7a49562b4368684351af892de62
unknown <islam15-3879@diu.edu.bd>
2022-05-04 19:21:16 +0600

    notfound-added

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Not Found !! There is no page available !!!</h2>
+        </div>
+    );
+};
+
+export default NotFound;
